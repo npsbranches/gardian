e7de0e6b5a89b3356f2db2bbbddb84cfa8310aeb
npsbranches <45974239+npsbranches@users.noreply.github.com>
2019-02-18 12:33:49 +0500

    2.1

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 $('.showcase-slider').slick({
     arrows: true,
-    dots: true
+    dots: true,
+    prevArrow: '<button class="prev"><i class="fa fa-angle-left"></i></button>',
+    nextArrow: '<button class="next"><i class="fa fa-angle-right"></i></button>'
 })
