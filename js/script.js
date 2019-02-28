514b0adc2e409b618ae1e7a8efa2f471c89771ce
npsbranches <45974239+npsbranches@users.noreply.github.com>
2019-02-28 12:24:39 +0500

    2.7

--- js/script.js
+++ js/script.js
@@ -4,3 +4,16 @@ $('.showcase-slider').slick({
     prevArrow: '<button class="prev"><i class="fa fa-angle-left"></i></button>',
     nextArrow: '<button class="next"><i class="fa fa-angle-right"></i></button>'
 })
+
+$('.btn-bars').click(function(){
+    $('.mobile-menu').toggleClass('toggle');
+    $('.btn-bars').toggleClass('fixed');
+    $('.logo').toggleClass('fixed');
+})
+
+$('.mobile-menu a').click(function(){
+    $('.mobile-menu').toggleClass('toggle');
+    $('.btn-bars').toggleClass('fixed');
+    $('.logo').toggleClass('fixed');
+})
+
